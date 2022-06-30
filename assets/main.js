54b6fda8ec36f680f3afb7d3703419716ac3230b
aristobulo99 <juniorespriella99@gmail.com>
2022-06-30 16:22:39 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+//import fetch from "node-fetch";
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCb8kq6qYo6QlFkudzRLx02g&part=snippet%2Cid&order=date&maxResults=2';
+
+const content = null || document.getElementById('content')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8d28b03acamsh1f852d928c7a155p103e95jsn2133845b6c9e',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI){
+    const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video =>`
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+        `).slice(0,4).join('')}
+
+    `;
+    content.innerHTML = view;
+    }catch (error){
+        console.error(error);
+    }
+})();
+
+
